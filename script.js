37f264808ee1423af7c357af0b647fa3867ee735
Hussain <k213584@nu.edu.pk>
2023-08-08 14:02:19 +0500

    Added a few fixes
    - Password input style is reset once the user inputs the correct password
    - script.js can now read input passwords

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // This code will prevent form submission if passwords are not the same
 
 const formObject = document.getElementById("signup-form");
+formObject.serialize
 const errorText = document.querySelectorAll(".error");
+let wrongPasswordTrigger = false;
 formObject.addEventListener('submit', (e) => {
-    console.log(formObject.elements['password'].textContent);
-    if (formObject.elements['password'] != formObject.elements['confirm_password']){
+    e.preventDefault();
+    let pass = formObject.elements['user_password'].value;
+    let conf_pass = formObject.elements['user_confirm_password'].value;
+    if (pass != conf_pass){
         e.preventDefault();
+        wrongPasswordTrigger = true;
         formObject.elements['password'].style.border = "1px solid red";
         formObject.elements['confirm_password'].style.border = "1px solid red";
         errorText.forEach((para) => {
@@ -13,4 +18,17 @@ formObject.addEventListener('submit', (e) => {
             para.style.color = "red";
         })
     }
+    else if(pass == conf_pass && wrongPasswordTrigger){
+        wrongPasswordTrigger = false;
+        resetFormStyles(formObject);
+    }
 });
+
+function resetFormStyles(formObj){
+    formObj.elements['password'].style.border = "1px solid black";
+    formObj.elements['confirm_password'].style.border = "1px solid black";
+    errorText.forEach((para) => {
+        para.textContent = "";
+        para.style.color = "black";
+    });
+}
